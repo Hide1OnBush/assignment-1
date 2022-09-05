ab687d80dacd5a53dfd0e9804c38958705a76016
Hide1OnBush <boqianshenluke@gmail.com>
2022-09-06 05:33:20 +1000

    Boundary and EQ testing code

--- tests/swen90006/mfa/BoundaryTests.java
+++ tests/swen90006/mfa/BoundaryTests.java
@@ -22,4 +22,229 @@ public class BoundaryTests
 	final int actual = 2;
 	assertEquals("Some failure message", expected, actual);
     }
+
+    // getData
+    @Test(expected = java.lang.IndexOutOfBoundsException.class)
+    public void getData_B1() throws Throwable
+    {
+        mfa.register("Tester", "qwER1234!!!", null);
+        mfa.login("Tester", "qwER1234!!!");
+        List<Integer> dataset1 = new ArrayList<>();
+        dataset1.add(10);
+        List<Integer> dataset2 = new ArrayList<>();
+        dataset2.add(1);
+        mfa.addData("Tester", dataset1);
+        mfa.addData("Tester", dataset2);
+        mfa.getData("Tester",-1);
+    }
+
+    @Test()
+    public void getData_B2() throws Throwable
+    {
+        mfa.register("Tester", "qwER1234!!!", null);
+        mfa.login("Tester", "qwER1234!!!");
+        List<Integer> dataset1 = new ArrayList<>();
+        dataset1.add(10);
+        List<Integer> dataset2 = new ArrayList<>();
+        dataset2.add(1);
+        mfa.addData("Tester", dataset1);
+        mfa.addData("Tester", dataset2);
+        mfa.getData("Tester",0);
+    }
+
+    @Test()
+    public void getData_B3() throws Throwable
+    {
+        mfa.register("Tester", "qwER1234!!!", null);
+        mfa.login("Tester", "qwER1234!!!");
+        List<Integer> dataset1 = new ArrayList<>();
+        dataset1.add(10);
+        List<Integer> dataset2 = new ArrayList<>();
+        dataset2.add(1);
+        List<Integer> dataset3 = new ArrayList<>();
+        dataset3.add(2);
+        mfa.addData("Tester", dataset1);
+        mfa.addData("Tester", dataset2);
+        mfa.addData("Tester", dataset3);
+        mfa.getData("Tester",1);
+    }
+
+    @Test()
+    public void getData_B4() throws Throwable
+    {
+        mfa.register("Tester", "qwER1234!!!", null);
+        mfa.login("Tester", "qwER1234!!!");
+        List<Integer> dataset1 = new ArrayList<>();
+        dataset1.add(10);
+        List<Integer> dataset2 = new ArrayList<>();
+        dataset2.add(1);
+        List<Integer> dataset3 = new ArrayList<>();
+        dataset3.add(2);
+        mfa.addData("Tester", dataset1);
+        mfa.addData("Tester", dataset2);
+        mfa.addData("Tester", dataset3);
+        mfa.getData("Tester",2);
+    }
+
+    @Test(expected = java.lang.IndexOutOfBoundsException.class)
+    public void getData_B5() throws Throwable
+    {
+        mfa.register("Tester", "qwER1234!!!", null);
+        mfa.login("Tester", "qwER1234!!!");
+        List<Integer> dataset1 = new ArrayList<>();
+        dataset1.add(10);
+        List<Integer> dataset2 = new ArrayList<>();
+        dataset2.add(1);
+        List<Integer> dataset3 = new ArrayList<>();
+        dataset3.add(2);
+        mfa.addData("Tester", dataset1);
+        mfa.addData("Tester", dataset2);
+        mfa.addData("Tester", dataset3);
+        mfa.getData("Tester",3);
+    }
+
+    // register
+
+    @Test()
+    public void register_B6() throws Throwable
+    {
+        mfa.register("Tester", "qwER1234!!!", null);
+    }
+
+    @Test()
+    public void register_B7() throws Throwable
+    {
+        mfa.register("Test", "qwER1234!!!", null);
+    }
+
+    @Test(expected = InvalidUsernameException.class)
+    public void register_B8() throws Throwable
+    {
+        mfa.register("Tes", "qwER1234!!!", null);
+    }
+
+    @Test(expected = InvalidUsernameException.class)
+    public void register_B9() throws Throwable
+    {
+        mfa.register("Tester@", "qwER1234!!!", null);
+    }
+
+    @Test()
+    public void register_B10() throws Throwable
+    {
+        mfa.register("TesterA", "qwER1234!!!", null);
+    }
+
+    @Test()
+    public void register_B11() throws Throwable
+    {
+        mfa.register("TesterB", "qwER1234!!!", null);
+    }
+
+    @Test()
+    public void register_B12() throws Throwable
+    {
+        mfa.register("TesterZ", "qwER1234!!!", null);
+    }
+
+    @Test(expected = InvalidUsernameException.class)
+    public void register_B13() throws Throwable
+    {
+        mfa.register("Tester[", "qwER1234!!!", null);
+    }
+
+    @Test(expected = InvalidUsernameException.class)
+    public void register_B14() throws Throwable
+    {
+        mfa.register("Tester`", "qwER1234!!!", null);
+    }
+
+    @Test()
+    public void register_B15() throws Throwable
+    {
+        mfa.register("Testera", "qwER1234!!!", null);
+    }
+
+    @Test()
+    public void register_B16() throws Throwable
+    {
+        mfa.register("Testerb", "qwER1234!!!", null);
+    }
+
+    @Test()
+    public void register_B17() throws Throwable
+    {
+        mfa.register("Testerz", "qwER1234!!!", null);
+    }
+
+    @Test(expected = InvalidUsernameException.class)
+    public void register_B18() throws Throwable
+    {
+        mfa.register("Tester{", "qwER1234!!!", null);
+    }
+
+    @Test(expected = InvalidPasswordException.class)
+    public void register_B19() throws Throwable
+    {
+        mfa.register("Tester", "qwER////!!!", null);
+    }
+
+    @Test()
+    public void register_B20() throws Throwable
+    {
+        mfa.register("Tester", "qwER0000!!!", null);
+    }
+
+    @Test()
+    public void register_B21() throws Throwable
+    {
+        mfa.register("Tester", "qwER1111!!!", null);
+    }
+
+    @Test()
+    public void register_B22() throws Throwable
+    {
+        mfa.register("Tester", "qwER9999!!!", null);
+    }
+
+    @Test(expected = InvalidPasswordException.class)
+    public void register_B23() throws Throwable
+    {
+        mfa.register("Tester", "qwER::::!!!", null);
+    }
+
+    @Test(expected = InvalidPasswordException.class)
+    public void register_B24() throws Throwable
+    {
+        mfa.register("Tester", "@@@@1234!!!", null);
+    }
+
+    @Test()
+    public void register_B25() throws Throwable
+    {
+        mfa.register("Tester", "qwAA1234!!!", null);
+    }
+
+    @Test()
+    public void register_B26() throws Throwable
+    {
+        mfa.register("Tester", "qwBB1234!!!", null);
+    }
+
+    @Test()
+    public void register_B27() throws Throwable
+    {
+        mfa.register("Tester", "qwZZ1234!!!", null);
+    }
+
+    @Test(expected = InvalidPasswordException.class)
+    public void register_B28() throws Throwable
+    {
+        mfa.register("Tester", "[[[[1234!!!", null);
+    }
+
+
+
+
+
 }
--- tests/swen90006/mfa/PartitioningTests.java
+++ tests/swen90006/mfa/PartitioningTests.java
@@ -1,6 +1,12 @@
 package swen90006.mfa;
 
 import org.junit.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import static org.junit.Assert.*;
 
 public class PartitioningTests
@@ -62,5 +68,250 @@ public class PartitioningTests
 	final int actual = 1 + 2;
 	//Uncomment the following line to make the test fail
 	//assertEquals("Some failure message", expected, actual);
+    }
+
+
+    //test: getData
+    @Test(expected = NoSuchUserException.class)
+    public void getData_EC_1() throws Throwable
+    {
+        List<Integer> dataset1 = new ArrayList<>();
+        dataset1.add(10);
+        List<Integer> dataset2 = new ArrayList<>();
+        dataset2.add(10);
+        mfa.addData("Tester", dataset1);
+        mfa.addData("Tester", dataset2);
+        mfa.getData("Tester",1);
+    }
+
+    @Test(expected = UnauthenticatedUserException.class)
+    public void getData_EC_2() throws Throwable
+    {
+        mfa.register("Tester", "qwER1234!!!", null);
+        List<Integer> dataset1 = new ArrayList<>();
+        dataset1.add(10);
+        List<Integer> dataset2 = new ArrayList<>();
+        dataset2.add(1);
+        mfa.addData("Tester", dataset1);
+        mfa.addData("Tester", dataset2);
+        mfa.getData("Tester",1);
+    }
+
+    @Test
+    public void getData_EC_3() throws Throwable
+    {
+        mfa.register("Tester", "qwER1234!!!", null);
+        mfa.login("Tester", "qwER1234!!!");
+        List<Integer> dataset1 = new ArrayList<>();
+        dataset1.add(10);
+        List<Integer> dataset2 = new ArrayList<>();
+        dataset2.add(1);
+        mfa.addData("Tester", dataset1);
+        mfa.addData("Tester", dataset2);
+        mfa.getData("Tester",1);
+
+        assertEquals(dataset2, mfa.getData("Tester", 1));
+
+    }
+
+    @Test(expected = java.lang.IndexOutOfBoundsException.class)
+    public void getData_EC_4() throws Throwable
+    {
+        mfa.register("Tester", "qwER1234!!!", null);
+        mfa.login("Tester", "qwER1234!!!");
+        List<Integer> dataset1 = new ArrayList<>();
+        dataset1.add(10);
+        List<Integer> dataset2 = new ArrayList<>();
+        dataset2.add(1);
+        mfa.addData("Tester", dataset1);
+        mfa.addData("Tester", dataset2);
+        mfa.getData("Tester",-1);
+    }
+
+    @Test(expected = java.lang.IndexOutOfBoundsException.class)
+    public void getData_EC_5() throws Throwable
+    {
+        mfa.register("Tester", "qwER1234!!!", null);
+        mfa.login("Tester", "qwER1234!!!");
+        List<Integer> dataset1 = new ArrayList<>();
+        dataset1.add(10);
+        List<Integer> dataset2 = new ArrayList<>();
+        dataset2.add(1);
+        mfa.addData("Tester", dataset1);
+        mfa.addData("Tester", dataset2);
+        mfa.getData("Tester",2);
+    }
+
+    @Test()
+    public void getData_EC_6() throws Throwable
+    {
+        mfa.register("Tester", "qwER1234!!!", "SWEN90006");
+        mfa.login("Tester", "qwER1234!!!");
+        mfa.isAuthenticated("Tester");
+        mfa.respondToPushNotification("Tester", "SWEN90006");
+        List<Integer> dataset1 = new ArrayList<>();
+        dataset1.add(10);
+        List<Integer> dataset2 = new ArrayList<>();
+        dataset2.add(1);
+        mfa.addData("Tester", dataset1);
+        mfa.addData("Tester", dataset2);
+        mfa.getData("Tester",1);
+
+        assertEquals(dataset2, mfa.getData("Tester", 1));
+    }
+
+    @Test(expected = java.lang.IndexOutOfBoundsException.class)
+    public void getData_EC_7() throws Throwable
+    {
+        mfa.register("Tester", "qwER1234!!!", "SWEN90006");
+        mfa.login("Tester", "qwER1234!!!");
+        mfa.isAuthenticated("Tester");
+        mfa.respondToPushNotification("Tester", "SWEN90006");
+        List<Integer> dataset1 = new ArrayList<>();
+        dataset1.add(10);
+        List<Integer> dataset2 = new ArrayList<>();
+        dataset2.add(1);
+        mfa.addData("Tester", dataset1);
+        mfa.addData("Tester", dataset2);
+        mfa.getData("Tester",-1);
+
+        assertEquals(dataset2, mfa.getData("Tester", -1));
+    }
+
+    @Test(expected = java.lang.IndexOutOfBoundsException.class)
+    public void getData_EC_8() throws Throwable
+    {
+        mfa.register("Tester", "qwER1234!!!", "SWEN90006");
+        mfa.login("Tester", "qwER1234!!!");
+        mfa.isAuthenticated("Tester");
+        mfa.respondToPushNotification("Tester", "SWEN90006");
+        List<Integer> dataset1 = new ArrayList<>();
+        dataset1.add(10);
+        List<Integer> dataset2 = new ArrayList<>();
+        dataset2.add(1);
+        mfa.addData("Tester", dataset1);
+        mfa.addData("Tester", dataset2);
+        mfa.getData("Tester",2);
+
+        assertEquals(dataset2, mfa.getData("Tester", 2));
+    }
+
+    // test: register
+    @Test(expected = DuplicateUserException.class)
+    public void register_EC_9() throws Throwable
+    {
+        mfa.register("Tester", "qwER1234!!!", null);
+        mfa.register("Tester", "qwER1234!!!", null);
+    }
+
+    @Test(expected = InvalidUsernameException.class)
+    public void register_EC_10() throws Throwable
+    {
+        mfa.register("Tes", "qwER1234!!!", null);
+    }
+
+    @Test(expected = InvalidUsernameException.class)
+    public void register_EC_11() throws Throwable
+    {
+        mfa.register("Tes12312", "qwER1234!!!", null);
+    }
+
+    @Test(expected = InvalidPasswordException.class)
+    public void register_EC_12() throws Throwable
+    {
+        mfa.register("Tester", "2131231231234!!!", null);
+    }
+
+    @Test(expected = InvalidPasswordException.class)
+    public void register_EC_13() throws Throwable
+    {
+        mfa.register("Tester", "qweRTYUUIO!!!", null);
+    }
+
+    @Test(expected = InvalidPasswordException.class)
+    public void register_EC_14() throws Throwable
+    {
+        mfa.register("Tester", "qweR1234", null);
+    }
+
+    @Test()
+    public void register_EC_15() throws Throwable
+    {
+        mfa.register("Tester", "qweR1234!!!", null);
+        assertTrue(mfa.isUser("Tester"));
+    }
+
+    @Test()
+    public void register_EC_16() throws Throwable
+    {
+        mfa.register("Tester", "qweR1234!!!", "SWEN90006");
+        assertTrue(mfa.isUser("Tester"));
+    }
+
+    // test login
+
+    @Test(expected = NoSuchUserException.class)
+    public void login_EC_17() throws Throwable
+    {
+        mfa.login("Tester", "qweR1234!!!");
+    }
+
+    @Test(expected = IncorrectPasswordException.class)
+    public void login_EC_18() throws Throwable
+    {
+        mfa.register("Tester", "qweR1234!!!", "SWEN90006");
+        mfa.login("Tester","qweR1234!");
+
+    }
+
+    @Test()
+    public void login_EC_19() throws Throwable
+    {
+        mfa.register("Tester", "qweR1234!!!", null);
+        mfa.login("Tester","qweR1234!!!");
+        mfa.isAuthenticated("Tester");
+
+        assertEquals(MFA.AuthenticationStatus.SINGLE, mfa.login("Tester","qweR1234!!!"));
+    }
+
+    @Test()
+    public void login_EC_20() throws Throwable
+    {
+        mfa.register("Tester", "qweR1234!!!", "SWEN90006");
+        mfa.login("Tester","qweR1234!!!");
+        mfa.isAuthenticated("Tester");
+        assertEquals(MFA.AuthenticationStatus.SINGLE, mfa.login("Tester","qweR1234!!!"));
+    }
+
+    // test respondToPushNotification
+
+    @Test(expected = NoSuchUserException.class)
+    public void respondToPushNotification_EC_21() throws Throwable
+    {
+        mfa.respondToPushNotification("Tester", "SWEN90006");
+    }
+
+    @Test()
+    public void respondToPushNotification_EC_22() throws Throwable
+    {
+        mfa.register("Tester", "qweR1234!!!", null);
+        assertEquals(MFA.AuthenticationStatus.NONE, mfa.respondToPushNotification("Tester","SWEN90006"));
+    }
+
+    @Test(expected = IncorrectDeviceIDException.class)
+    public void respondToPushNotification_EC_23() throws Throwable
+    {
+        mfa.register("Tester", "qweR1234!!!", "SWEN90006");
+        mfa.login("Tester", "qweR1234!!!");
+        mfa.respondToPushNotification("Tester", "SWEN90007");
+    }
+
+    @Test()
+    public void respondToPushNotification_EC_24() throws Throwable
+    {
+        mfa.register("Tester", "qweR1234!!!", "SWEN90006");
+        mfa.login("Tester", "qweR1234!!!");
+        mfa.respondToPushNotification("Tester", "SWEN90006");
+        assertEquals(MFA.AuthenticationStatus.DOUBLE, mfa.respondToPushNotification("Tester","SWEN90006"));
     }
 }
